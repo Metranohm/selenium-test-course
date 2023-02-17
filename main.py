0126fb8d287d57a8cf5376c056cf418f92f9ccba
Metranohm <andrewwink@gmail.com>
2023-02-16 21:46:44 -0600

    update drivers

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
+import os
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+
+
+s = Service('usr/local/bin/chromedriver')
+driver = webdriver.Chrome(service=s) 
